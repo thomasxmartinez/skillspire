37019280180544194e7ac3f384878bb206d0fe2a
Thien <h.Thien10@yahoo.com>
2018-04-14 12:05:42 -0700

    jkfejkf

---
+++ thienhoang/class4.18/app.js
@@ -0,0 +1,7 @@
+
+
+alert('hello world');
+let firstApples = prompt("enter 1st amount");
+let secondApples = prompt("enter  2nd amount");
+let sumOfApples = parseInt(firstApples) + parseInt(secondApples);
+alert(sumOfApples);
---
+++ thienhoang/index.js
@@ -0,0 +1,1 @@
+alert('say something')
