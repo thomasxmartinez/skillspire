508e37d73cc3c885ef0fe1dd1f2e1c251e17ebe6
rttgg <rttgg1@gmail.com>
2018-04-14 02:40:08 -0700

    my week4 assignment update

--- RomanGebrehiwot/romangebrehiwot/week4Assign.js
+++ RomanGebrehiwot/romangebrehiwot/week4Assign.js
@@ -4,35 +4,5 @@ function areaClass(lenth, height) {
      
   }
   areaClass(3,4);
-<<<<<<< HEAD
-/*
-    var areaClass = document.getElementById('area-class')
-    areaClass.addEventListener('submit', function(){
-    var length = document.getElementById('alength-input');
-    var height = document.getElementById('aheight-input');
-    return areaClass;
-   }
-    
-/*
 
-  'use strict'
-
-var nameForm = document.getElementById('name-form')
-nameForm.addEventListener('submit', function(){
-    var fnameInput = document.getElementById('fname-input');
-    var lnameInput = document.getElementById('lname-input');
-    var outputP = document.getElementById('output-p');
-
-    event.preventDefault();
-    if (fnameInput.value === '' || lnameInput.value === '') {
-        outputP.innerHTML = 'you need to enter your first name and last name.';
- } else {
-     outputP.innerHTML = 'Welcome, ' + fnameInput.value + '' + lnameInput.value + '.';  
  
- fnameInput.value = '';
-lnameInput.value = '';
-}
-
-})*/
-=======
->>>>>>> romang
