7ed905942660868c6359b6a6235a4f04c02bb8a3
thomasxmartinez <thomasxmartinez@gmail.com>
2018-05-17 09:16:12 -0700

    ayo

--- teachstuff/WeekNine/lesson-2/src/App.js
+++ teachstuff/WeekNine/lesson-2/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Welcome from './newFile.js'; //add new file index.js with a class
+import Welcome from './newFile.js'; //add new file newFile.js with a class
 
 class App extends Component {
   render() {
