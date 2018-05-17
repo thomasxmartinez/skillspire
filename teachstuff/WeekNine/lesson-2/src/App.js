d9528aee67e73f501a5a78110fe6162726a8e0de
tommytnez <tommytnez@yahoo.com>
2018-05-16 18:18:00 -0700

    react stuff

---
+++ teachstuff/WeekNine/lesson-2/src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Welcome from './newFile.js'; //add new file index.js with a class
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Welcome name="Thomas" />
+      </div>
+    );
+  }
+}
+
+export default App;
