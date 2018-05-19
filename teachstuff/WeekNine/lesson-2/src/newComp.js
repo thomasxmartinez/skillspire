a8a4c3fad96d85adb581cc106ea3397de04205d8
tommytnez <tommytnez@yahoo.com>
2018-05-19 09:08:38 -0700

    yoyo

--- teachstuff/WeekNine/lesson-2/src/newComp.js
+++ teachstuff/WeekNine/lesson-2/src/newComp.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+//tagged template literal valid js
+const Wrapper = styled.div`
+  width: 32%;
+  border: 2px solid #333;
+  border-radius: 4px;
+  margin-bottom: 25px;
+  padding-right: 10px;
+  overflow: hidden;
+`;
+
 const Wowza = () => (
-  <div className="wowza">
+  <Wrapper className="wowza">
     <h1> wowzers</h1>
     <input type="text" placeholder="Search" />
     <Link to="/search"> or Browza</Link>
-  </div>
+    {/*
+  generates anchor tag with correct url
+*/}
+  </Wrapper>
 );
 
 export default Wowza;
