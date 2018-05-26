e7b7f9ec18a73be0affa45b399c3b001cf7641fa
weakcode <h.thien10@yahoo.com>
2018-05-26 07:07:47 -0700

    class working with json

---
+++ thienhoang/class/5.9/lesson-1/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const h1 = <h1>Skillsdgpire is the fun!!!!</h1>
+function formatName (user) {
+  return user.firstName + ' ' + user.lastName + ' is not your instructor';
+}
+
+const user = {
+  firstName: 'Kobe',
+  lastName: 'Bryant'
+};
+
+const element = <h1>look at me do JSX {formatName(user)}</h1>
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Jungle</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <section> {h1}{element}</section>
+
+      </div>
+    );
+  }
+}
+
+export default App;
