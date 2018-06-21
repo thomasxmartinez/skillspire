6ba844f0df6f1803e95118f9c4b0e460f687a791
weakcode <h.thien10@yahoo.com>
2018-06-20 18:50:12 -0700

    fef

--- thienhoang/class/5.30/server.js
+++ thienhoang/class/5.30/server.js
@@ -7,6 +7,11 @@ var port = process.env.PORT || 3000;
 app.use('/', function(a, b){
     b.send('hey team dsfsdhey!!!!')
 })
+app.use(express.static(__dirname + '/app/'));
+
+app.use( function(req, res, next){
+    res.status(404).sendfile(__dirname + '/app/index.htmll')
+})
 
 app.listen(port, function(){
     console.log('party at port' + port)
