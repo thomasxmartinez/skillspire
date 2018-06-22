34b53b566adaa6fed2c205e8f37f49a1219cb165
Thien <h.Thien10@yahoo.com>
2018-06-22 10:47:29 -0700

    glossapp

--- thienhoang/other homework/glossApp/glossapp/src/button.jsx
+++ thienhoang/other homework/glossApp/glossapp/src/button.jsx
@@ -1,11 +1,15 @@
 import React, { Component  } from "react";
 import "./glossary.jsx"
+import jsonGloss from "./glossary.jsx";
 
 
 class Button extends Component{
 
  resultAll = () => {
-    document.getElementById('results').innerHTML = "hello" 
+
+    
+    document.getElementById('resultsletter').innerHTML = JSON.stringify(jsonGloss, ['firstLetter'])
+    document.getElementById('resultsdef').innerHTML = JSON.stringify(jsonGloss, ['Def'])
 }
 
 
@@ -15,7 +19,18 @@ render(){
             <button onClick={ this.resultAll }>press
 
             </button>
-            <p id='results'></p>
+            <ul><li>
+                <tr>
+                    <td><p id='resultsletter'></p>
+                    </td>
+                </tr>
+                <tr>
+                    <td><p id='resultsdef'></p>
+                    </td>
+                </tr>       
+                </li>
+            </ul>
+            
         </div>
 
     )
