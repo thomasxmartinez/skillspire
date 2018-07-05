925e03e0a64f9b64237404296ffb7806146faa92
Thien <h.Thien10@yahoo.com>
2018-07-05 09:58:12 -0700

    flascard done

--- thienhoang/other homework/react mini project/react-flashcards/src/Card/card.jsx
+++ thienhoang/other homework/react mini project/react-flashcards/src/Card/card.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import './card.css';
+import './Card.css';
 
 const Card = (props) => (
-    <div className='card-container'>
-        <div className='card'>
-            <div className='front'>
-                <div className='eng'>{props.eng}</div>
+    <div className="card-container">
+        <div className="card">
+            <div className="front">
+                <div className="eng">{props.eng}</div>
             </div>
-            <div className='back'>
-                <div className='han'>{props.han}</div>
-                <div className='pin'>{props.pin}</div>
+            <div className="front back">
+                <div className="han">{props.han}</div>
+                <div className="pin">{props.pin}</div>
             </div>
         </div>
-
     </div>
 )
 
-
-export default Card;
+export default Card
--- thienhoang/other homework/react mini project/react-flashcards/src/DrawButton/drawButton.jsx
+++ thienhoang/other homework/react mini project/react-flashcards/src/DrawButton/drawButton.jsx
@@ -1,29 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './DrawButton.css';
 
-
-class DrawButton extends Component {
+class DrawButton extends Component{
     constructor(props){
         super(props);
-        this.drawcard = this.drawCard.bind(this);
+
+        this.drawCard = this.drawCard.bind(this);
     }
-
-
 
     drawCard(){
         this.props.drawCard();
     }
 
-
     render(props){
         return(
             <div className="buttonContainer">
-                <button className="btn" onClick={this.drawCard}> Draw Card</button>
+                <button className="btn" onClick={this.drawCard}>Draw Card</button>
             </div>
         )
     }
-
 }
 
-
-export default DrawButton;
+export default DrawButton
